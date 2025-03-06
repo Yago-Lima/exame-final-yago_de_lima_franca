0d958c2e3c09e9b08650dfe999382073ed49d9f0
Yago de Lima França <134020465+Yago-Lima@users.noreply.github.com>
2025-03-06 20:38:36 -0300

    Update CompressionType.java

--- src/main/java/io/github/manoelcampos/stringcompression/CompressionType.java
+++ src/main/java/io/github/manoelcampos/stringcompression/CompressionType.java
@@ -1,5 +1,8 @@
 package io.github.manoelcampos.stringcompression;
-
+/**
+ * Enumeração com os tipos de compressão disponíveis.
+ * @author Yago de Lima França
+ */
 public enum CompressionType {
     RLE,
     WORD_SUBSTITUTION;
