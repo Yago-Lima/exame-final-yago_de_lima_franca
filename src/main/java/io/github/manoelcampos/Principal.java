1843fe34c013a92158f5efa0bee3fa0d4887e78b
Yago de Lima França <134020465+Yago-Lima@users.noreply.github.com>
2025-03-06 19:58:09 -0300

    Update Principal.java
    alterada a classe principal permitindo que ela selecione o compressor RLE

--- src/main/java/io/github/manoelcampos/Principal.java
+++ src/main/java/io/github/manoelcampos/Principal.java
@@ -1,14 +1,35 @@
 package io.github.manoelcampos;
 
- /*
+import io.github.manoelcampos.stringcompression.CompressionFactory;
+import io.github.manoelcampos.stringcompression.CompressionStrategy;
+
+/*
  * Classe para implementar o uso dos compressores de texto após
  * a aplicação do(s) padrão(ões) de projeto que se enquadre(m) com o contexto apresentado no README.
  *
  * @author Yago de Lima França
 */
-public class Principal {
-    public static void main(final String[] args) {
-        // TODO: Implemente o uso das classes criadas/alteradas aqui.
-        // Não se esqueça de garantir que os testes na pasta `/test` continuam funcionando.
-    }
-}
+ public class Principal {
+     private CompressionStrategy compressionStrategy;
+
+     public void setCompressionAlgorithm(String algorithmName) {
+         this.compressionStrategy = CompressionFactory.getCompressionAlgorithm(algorithmName);
+     }
+
+     public String compressData(String data) {
+         return compressionStrategy.compress(data);
+     }
+
+     public String decompressData(String data) {
+         return compressionStrategy.decompress(data);
+     }
+
+     public static void main(String[] args) {
+         Principal principal = new Principal();
+         principal.setCompressionAlgorithm("RLE");
+         String compressedData = principal.compressData("aaaaaaaaaaaaaaaaabbbbbbababababacabaaaaaaaaaaaaaaaaccccccccccccccaaaaaaaaa");
+         String decompressedData = principal.decompressData(compressedData);
+         System.out.println("Compressed: " + compressedData);
+         System.out.println("Decompressed: " + decompressedData);
+     }
+ }
